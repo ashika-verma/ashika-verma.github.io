78cc64bae25b1d81ab592923cdb78a5deb23cdc8
Ashika Verma <ashikav2000@gmail.com>
2022-12-30 14:39:11 -0600

    update content

--- script.js
+++ script.js
@@ -279,3 +279,38 @@ const toggleIdVisibility = (id) => {
   document.getElementById(id).style["display"] =
     document.getElementById(id).style["display"] === "block" ? "none" : "block";
 };
+
+// -----------------------  KONAMI CODE ---------------------------
+
+var pattern = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a",
+];
+var index = 0;
+document.addEventListener("keydown", function onEvent(event) {
+  if (event.key === pattern[index]) {
+    index++;
+    if (index === pattern.length) {
+      index = 0;
+      konami();
+    }
+  } else {
+    index = 0;
+  }
+});
+
+function konami() {
+  alert("hello!");
+}
+
+function openURL(url) {
+  window.open(url, "_blank");
+}
