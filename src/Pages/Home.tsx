41c094f80f728703678d2e067d910dd3fc92b345
Ashika Verma <ashikav2000@gmail.com>
2023-12-31 14:11:45 -0600

    add tailwind

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,7 +5,7 @@ import Navbar from "../Navbar";
 const HomePage: React.FC = () => {
   return (
     <div>
-      <h2>Home Page</h2>
+      <h2 className="bg-teal-500">Home Page</h2>
       <Navbar></Navbar>
     </div>
   );
