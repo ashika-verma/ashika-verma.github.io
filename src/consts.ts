e3efb135b05654a4d87b92c9d573aaf12337b0f5
Ashika Verma <ashikav2000@gmail.com>
2024-04-20 14:18:00 -0400

    update resume

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 export const HOME_URL = "/";
 export const PROJECTS_URL = "/projects";
 export const CLASSES_URL = "/classes";
-export const RESUME_URL = "./files/Ashika_Verma_Resume_11_12_2023.pdf";
+export const RESUME_URL =
+  "./files/public/files/Ashika_Verma_Resume_04_20_2024.pdf";
 export const GITHUB_URL = "https://github.com/ashika-verma";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/ashika-verma/";
