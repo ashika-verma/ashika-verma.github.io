6398dbdca86db11a716ab7d3f357f89454bd52c3
Ashika Verma <ashikav2000@gmail.com>
2025-08-16 13:33:09 -0400

    update resume URL to latest version and add new resume files

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,6 @@ export const PROJECTS_URL = "/projects";
 export const CLASSES_URL = "/classes";
 export const BEST_BUTTON = "/button";
 export const BUSINESS_CARD = "/business-card";
-export const RESUME_URL = "./files/Ashika_Verma_Resume_09_14_2024.pdf";
+export const RESUME_URL = "./files/Ashika_Verma_Resume_08_16_2025.pdf";
 export const GITHUB_URL = "https://github.com/ashika-verma";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/ashika-verma/";
